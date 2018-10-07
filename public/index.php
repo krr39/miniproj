4d8f410f4ad43ff4be149dc388ac1349fe528381
krutika <krr39@njit.edu>
2018-10-06 23:31:50 -0400

    new csv file created and read

--- public/index.php
+++ public/index.php
@@ -1,69 +1,27 @@
 <?php
-echo 'test123';
-/**
- * Created by PhpStorm.
- * User: krutika
- * Date: 10/6/18
- * Time: 12:27 AM
- */
-main::start("example.csv");
-class main{
-    static public function start($filename)
-    {
-        $records=csv::getRecords($filename);
-        //$record= recordFactory::create();
-        //print_r($records);
+class parsecsv{
+    function read_csv($filename){
+        $data = file_get_contents($filename);
+        $data = explode("\r\n", $data);
+        $data = array_filter($data);
 
-    }
-}
-
-class csv{
-    static public function getRecords($filename){
-        $file=fopen($filename,"r");
-        $fieldNames=array();
-        $count=0;
-        while(! feof($file))
-        {
-            $record= fgetcsv($file);
-            if($count == 0)
-            {
-                $fieldNames[]=$record;
-            }
-            else{
-                $records[]=recordFactory::create($fieldNames, $record);
-            }
-            $count++;
-
+        foreach($data as $key => $d){
+            $d = str_getcsv ($d, ",", '"' , "\\");
+            $data[$key] = $d;
         }
-        fclose($file);
-        return $records;
 
+        return $data;
     }
-}
-class record{
-    public function _construct(Array $fieldNames=null,$values =  null)
-    {
 
-                $record= array_combine($fieldNames,$values);
-        print_r($record);
-        $this->createProperty();
-
-    }
-    public function createProperty($name='First',$value='Krutika')
-    {
-        $this->{$name} = $value;
+    public function start($filename){
+        return $this->read_csv($filename);
     }
 
 }
-class recordFactory{
 
-    public static function create(Array $fieldNames=null,Array $values = null){
 
-        $record=new record($fieldNames,$values);
-
-        return $record;
-}
-
-}
-
-class html{}
+$parsecsv = new parsecsv();
+echo "<pre>";
+$html = $parsecsv->start("us-50.csv");
+print_r($html);
+?>
--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
-#my mini project for IS601
+#my mini project for IS601cho 'test123';
+                          /**
+                           * Created by PhpStorm.
+                           * User: krutika
+                           * Date: 10/6/18
+                           * Time: 12:27 AM
+                           */
+                          main::start("example.csv");
+                          class main{
+                              static public function start($filename)
+                              {
+                          
+                          
+                              }
+                          }
+                          
+                          class csv{
+                              static public function readRecordFromCSV($filename)
+                              {
+                          
+                          
+                              }
+                          
+                          }
+                          class html{
+                          
+                          }
